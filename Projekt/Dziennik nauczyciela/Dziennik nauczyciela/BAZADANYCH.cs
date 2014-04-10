17a1e5b7ec5de0605a38d5b32ab62138675bf962
Thompkinss <thompkinss@gmail.com>
2014-04-11 00:36:16 +0200

    zad.dom. nr 3

---
+++ Projekt/Dziennik nauczyciela/Dziennik nauczyciela/BAZADANYCH.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziennik_nauczyciela
+{
+    public class BAZADANYCH
+    {
+        public class uczen
+        {
+            public int uczenID { get; set; }
+            public string imie { get; set; }
+            public string nazwisko { get; set; }
+            public string pesel { get; set; }
+            public int klasaNR { get; set; }
+        }
+
+        public class nauczyciel
+        {
+            public int nauczycielID {get; set; }
+            public string login { get; set; }
+            public string haslo { get; set; }
+            public string imie { get; set; }
+            public string nazwisko { get; set; }
+            public string email { get; set; }
+            public string email_haslo { get; set; }
+            public int zalogowany_mail { get; set; }
+        }
+
+        public class klasa
+        {
+            public int klasaID { get; set; }
+            public string nazwa { get; set; }
+            public string rocznik { get; set; }
+            public int nauczycielNR { get; set; }
+            public string haslo { get; set; }
+        }
+    }
+}
