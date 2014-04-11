e24e29c112d08381a48b22ea7627b9978b8dc713
Marek <marekwozniak1992@gmail.com>
2014-04-11 23:58:08 +0200

    KOMENTARZ #1

--- Projekt/Dziennik nauczyciela/Dziennik nauczyciela/Program.cs
+++ Projekt/Dziennik nauczyciela/Dziennik nauczyciela/Program.cs
@@ -14,6 +14,7 @@ namespace Dziennik_nauczyciela
         [STAThread]
         static void Main()
         {
+            //dodalem komentarz - do nauki sourceTree
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new fStart());
